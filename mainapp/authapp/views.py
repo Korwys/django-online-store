3e65c6281a94b880cf8ccf4d647e73d0b0403038
Daniil <sidorenkodk@gmail.com>
2022-07-23 21:22:00 +0300

    add: (authapp)  - login() controller  - template

---
+++ mainapp/authapp/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+
+def login(request):
+    context = {}
+    return render(request, 'authapp/login.html', context)
--- mainapp/mainapp/settings.py
+++ mainapp/mainapp/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
 
     'mainapp',
     'productapp',
+    'authapp',
 ]
 
 MIDDLEWARE = [
