c6bdc63f6d32b643f43dcb21404000f807756f16
Daniil <sidorenkodk@gmail.com>
2022-07-27 18:56:12 +0300

    add: (authapp) verifacation template, and same contreller, url

--- mainapp/authapp/urls.py
+++ mainapp/authapp/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import user_login, user_register
+from .views import user_login, new_user_registration, confirm_new_user_registration
 
 app_name = 'authapp'
+
 urlpatterns = [
     path('login/', user_login, name='user_login'),
-    path('register/', user_register, name='user_register')
+    path('register/', new_user_registration, name='new_user_registration'),
+    path('confirm_registration/<email>/<activation_key>', confirm_new_user_registration,
+         name='confirm_new_user_registration')
 
 ]
--- mainapp/authapp/views.py
+++ mainapp/authapp/views.py
@@ -1,41 +1,39 @@
-
-
 from django.shortcuts import render, HttpResponseRedirect
 from django.contrib import auth
 from django.urls import reverse
 
 from .forms import UserLoginForm, UserRegisterForm
+from .models import User
+from .services.crud import save_new_user_data, activate_new_user
 
 
 def user_login(request):
-    # login_form = UserLoginForm(request)
     login_form = UserLoginForm(data=request.POST)
     if request.method == 'POST' and login_form.is_valid():
         username = request.POST['username']
         password = request.POST['password']
-
         user = auth.authenticate(username=username, password=password)
         if user and user.is_active:
             auth.login(request, user)
             return HttpResponseRedirect(reverse('index'))
-    content = {'login_form': login_form}
-    return render(request, 'authapp/login.html', content)
+    context = {'login_form': login_form}
+    return render(request, 'authapp/login.html', context)
 
 
-
-
-def user_register(request):
+def new_user_registration(request):
     if request.method == 'POST':
-        register_form = UserRegisterForm(request.POST, request.FILES)
-
-        if register_form.is_valid:
-            user = register_form.save()
-        else:
-            return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+        return save_new_user_data(request)
     else:
-        register_form = UserRegisterForm()
-
-    context = {
-        'register_form': register_form,
+        context = {
+            'register_form': UserRegisterForm(),
         }
     return render(request, 'authapp/register.html', context)
+
+
+def confirm_new_user_registration(request, email, activation_key):
+    """ Отображает юзеру результат верификации на сайте после перехода по ссылке из отправленого нами письма"""
+
+    contex = {
+        'activation_result_info': activate_new_user(request, email, activation_key)
+    }
+    return render(request, 'authapp/verification.html', contex)
